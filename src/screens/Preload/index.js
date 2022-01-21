598906d6774eda6e9fab517242f6f7725b9439dd
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-21 14:25:54 -0300

    verifica token e navega para tela SignIn

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import {Container, LoadingIcon} from './styles';
+import React, {useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import {useNavigation} from '@react-navigation/native';
 
+import {Container, LoadingIcon} from './styles';
 import BarberLogo from '../../assets/barber.svg';
 
 export default () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('token');
+
+      if (token) {
+        // validar o token
+      } else {
+        navigation.navigate('SignIn');
+      }
+    };
+
+    checkToken();
+  }, []);
+
   return (
     <Container>
       <BarberLogo width="100%" height="160" />
