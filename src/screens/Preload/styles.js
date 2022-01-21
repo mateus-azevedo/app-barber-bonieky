c27b2c71ae49fe3b9ce4d901fdbfc0b7c7dd30c8
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-20 21:28:15 -0300

    criada tela de preload

--- src/screens/Preload/styles.js
+++ src/screens/Preload/styles.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #63c2d1;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 50px;
+`;
