909591fa1a7adb000287aacff968b064ae822cc8
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-20 21:27:44 -0300

    configs Stack.Navigator

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -8,7 +8,11 @@ import SignOut from '../screens/SignOut';
 const Stack = createStackNavigator();
 
 export default () => (
-  <Stack.Navigator>
+  <Stack.Navigator
+    initialRouteName="Preload"
+    screenOptions={{
+      headerShown: false,
+    }}>
     <Stack.Screen name="Preload" component={Preload} />
     <Stack.Screen name="SignIn" component={SignIn} />
     <Stack.Screen name="SignUp" component={SignOut} />
