d99034e1de932da187aa13f283622c5c0c752283
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-21 23:19:49 -0300

    requisicao do token - manda um novo token

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,20 @@
 const BASE_API = 'https://api.b7web.com.br/devbarber/api';
 
 export default {
-  checkToken: async () => {},
+  checkToken: async token => {
+    const req = await fetch(`${BASE_API}/auth/refresh`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({token}),
+    });
+
+    const json = await req.json();
+
+    return json;
+  },
 
   signIn: async (email, password) => {
     const req = await fetch(`${BASE_API}/auth/login`, {
