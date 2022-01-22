e54f075c6f216c1327cf4c03990a93998ac6a11d
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-21 23:17:20 -0300

    requisicao de cadastro de usuario

--- src/Api.js
+++ src/Api.js
@@ -18,5 +18,18 @@ export default {
     return json;
   },
 
-  signUp: async () => {},
+  signUp: async (name, email, password) => {
+    const req = await fetch(`${BASE_API}/user`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name, email, password}),
+    });
+
+    const json = await req.json();
+
+    return json;
+  },
 };
