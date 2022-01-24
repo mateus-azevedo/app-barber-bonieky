6b1887f7941473050b4f011968a336087b5b2054
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-24 17:18:48 -0300

    icone do centro na tab bar
    2:33:10

--- src/components/CustomTabBar.js
+++ src/components/CustomTabBar.js
@@ -19,6 +19,18 @@ const TabItem = styled.TouchableOpacity`
   align-items: center;
 `;
 
+const TabItemCenter = styled.TouchableOpacity`
+  width: 70px;
+  height: 70px;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 35px;
+  border: 3px solid #4eadbe;
+
+  margin-top: -20px;
+`;
+
 export default ({state, navigation}) => {
   const goTo = screenName => {
     navigation.navigate(screenName);
@@ -44,14 +56,9 @@ export default ({state, navigation}) => {
         />
       </TabItem>
 
-      <TabItem onPress={() => goTo('Appointments')}>
-        <TodayIcon
-          style={{opacity: state.index === 2 ? 1 : 0.5}}
-          width="24"
-          height="24"
-          fill="#FFF"
-        />
-      </TabItem>
+      <TabItemCenter onPress={() => goTo('Appointments')}>
+        <TodayIcon width="32" height="32" fill="#4eadbe" />
+      </TabItemCenter>
 
       <TabItem onPress={() => goTo('Favorites')}>
         <FavoriteIcon
