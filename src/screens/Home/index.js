830aa9b3073de4e34995d99fccd179a41f891254
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-13 23:06:17 -0300

    criado o header da home e seus componentes

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import {useNavigation} from '@react-navigation/native';
+import {
+  Container,
+  Scroller,
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+} from './styles';
+
+import SearchIcon from '../../assets/search.svg';
+import MyLocationIcon from '../../assets/my_location.svg';
 
 export default () => {
+  const navigation = useNavigation();
+
   return (
     <Container>
-      <Text>Home</Text>
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle numberOflines={2}>
+            Encontre o seu barbeiro favorito
+          </HeaderTitle>
+          <SearchButton onPress={() => navigation.navigate('Search')}>
+            <SearchIcon width="26" height="26" fill="#FFFFFF" />
+          </SearchButton>
+        </HeaderArea>
+
+        <LocationArea>
+          <LocationInput
+            placeholder="Onde você está?"
+            placeholderTextColor="#ffffff"
+          />
+          <LocationFinder>
+            <MyLocationIcon width="24" height="24" fill="#FFFFFF" />
+          </LocationFinder>
+        </LocationArea>
+      </Scroller>
     </Container>
   );
 };
